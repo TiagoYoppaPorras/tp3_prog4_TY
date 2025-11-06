8696bd9b2da830b69aa757177f9ca461c370e8b8
tiagoYoppaPorras <tiagoyoppa@gmail.com>
2025-11-06 14:53:04 -0300

    subiendo script de la BD en archivo db.sql

--- backend/db.sql
+++ backend/db.sql
@@ -1,32 +1,41 @@
-CREATE DATABASE GestorMedico;
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(25) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `hash_contraseña` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-USE GestorMedico;
+CREATE TABLE `pacientes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(40) NOT NULL,
+  `apellido` varchar(40) NOT NULL,
+  `dni` int NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `obra_social` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE Paciente (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL,
-    apellido VARCHAR(50) NOT NULL,
-    DNI VARCHAR(15) UNIQUE NOT NULL,
-    fecha_nacimiento DATE NOT NULL,
-    obra_social VARCHAR(100)
-);
+CREATE TABLE `medicos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(40) NOT NULL,
+  `apellido` varchar(40) NOT NULL,
+  `especialidad` varchar(40) NOT NULL,
+  `matricula_profesional` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE Medico (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL,
-    apellido VARCHAR(50) NOT NULL,
-    especialidad VARCHAR(100) NOT NULL,
-    matricula_profesional VARCHAR(30) UNIQUE NOT NULL
-);
-
-CREATE TABLE Turno (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    paciente_id INT NOT NULL,
-    medico_id INT NOT NULL,
-    fecha DATE NOT NULL,
-    hora TIME NOT NULL,
-    estado ENUM('pendiente', 'atendido', 'cancelado') DEFAULT 'pendiente',
-    observaciones TEXT,
-    FOREIGN KEY (paciente_id) REFERENCES Paciente(id) ON DELETE CASCADE,
-    FOREIGN KEY (medico_id) REFERENCES Medico(id) ON DELETE CASCADE
-);
+CREATE TABLE `turnos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `paciente_id` int NOT NULL,
+  `medico_id` int NOT NULL,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  `estado` varchar(40) NOT NULL,
+  `observaciones` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_turnos_paciente_id_idx` (`paciente_id`),
+  KEY `fk_turnos_medico_id_idx` (`medico_id`),
+  CONSTRAINT `fk_turnos_medico_id` FOREIGN KEY (`medico_id`) REFERENCES `medicos` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_turnos_paciente_id` FOREIGN KEY (`paciente_id`) REFERENCES `pacientes` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
